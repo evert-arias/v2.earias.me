be950004a9424e7bdff4fd6b6db188c01fafa1ae
Evert Arias <evert.arias@hotmail.com>
2020-11-17 23:02:57 -0500

    🎉 Created Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hero"></div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style>
+.hero {
+  @apply bg-secondary-500 w-full h-64;
+}
+</style>
