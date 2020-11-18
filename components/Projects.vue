f6e8ab3f9bc77114ac0637b1c9980baf8295b4ec
Evert Arias <evert.arias@hotmail.com>
2020-11-17 23:03:08 -0500

    🎉 Created Projects component

---
+++ components/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="projects-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+}
+</script>
+
+<style>
+.projects-wrapper {
+  @apply bg-tertiary-700 w-full h-64;
+}
+</style>
