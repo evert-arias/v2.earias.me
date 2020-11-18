564d171106b9955cfcdaafc349edff940ef70b26
Evert Arias <evert.arias@hotmail.com>
2020-11-17 23:04:43 -0500

    🎨 Update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
-    <Nuxt />
+    <Header />
+    <div class="content-wrapper">
+      <div class="content-divider-top">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div class="content">
+        <Nuxt />
+      </div>
+    </div>
   </div>
 </template>
-
+<script>
+import Header from '~/components/Header.vue'
+export default {
+  components: { Header },
+}
+</script>
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-size: 18px;
   box-sizing: border-box;
 }
 
@@ -22,34 +40,35 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+a {
   text-decoration: none;
-  padding: 10px 30px;
+  cursor: pointer;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.content-wrapper {
+  @apply relative;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+.content-divider-top {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.content-divider-top svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 35px;
+}
+
+.content-divider-top .shape-fill {
+  fill: #3d5a80;
 }
 </style>
